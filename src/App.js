b6fae5b038e66d81ad71fafcd1d54ff275850cde
James Morris <70540466+jleomorris@users.noreply.github.com>
2021-01-30 12:05:25 +0000

    Update app functions to ES6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { useState } from 'react';
 // Styling
 import './App.scss';
-// Components
-import Slider from './components/Slider';
 //Images 
 import cat_cactus from './img/cat_cactus.jpg';
-import china from './img/china.jpg';
-import coast from './img/coast.jpg';
-import foliage from './img/foliage.jpg';
-import forest from './img/forest.jpg';
-import plant_shelf from './img/plant_shelf.jpg';
-import tree from './img/tree.jpg';
-import windsurfing from './img/windsurfing.jpg';
+// Components
+import ImageGallery from './components/ImageGallery';
+import ImageEditor from './components/ImageEditor';
 
 const DEFAULT_OPTIONS = [
   {
@@ -86,14 +80,13 @@ const DEFAULT_OPTIONS = [
   },
 ]
 
-function App() {
+const App = () => {
+  // State
   const [options, setOptions] = useState(DEFAULT_OPTIONS);
   const [selectedOptionIndex, setSelectedOptionSelectedIndex] = useState(0);
-  // const selectedOption = options[selectedOptionIndex];
   const [backgroundUrl, setBackgroundUrl] = useState(cat_cactus);
 
-  function handleSliderChange({ target }) {   
-    // Look through options and change the value of the selected option
+  const handleSliderChange = ({ target }) => {   
     setOptions(prevOptions => {
       return prevOptions.map((option) => {
         if (option.name !== target.name) return option;
@@ -102,7 +95,7 @@ function App() {
     })
   }
 
-  function getImageStyle() {
+  const getImageStyle = () => {
     const filters = options.map(option => {
       return `${option.property}(${option.value}${option.unit})`;
     })
@@ -110,16 +103,16 @@ function App() {
     return { filter: filters.join(' ') };
   }
 
-  function changeImageHandler(e) {
+  const changeImageHandler = (e) => {
     setBackgroundUrl(e.target.src);
     resetFilters();
   }
 
-  function resetFilters() {
+  const resetFilters = () => {
     setOptions(DEFAULT_OPTIONS);
   }
 
-  function handleResetButtonClick() {
+  const handleResetButtonClick = () => {
     resetFilters();
   }
 
@@ -129,42 +122,8 @@ function App() {
         <h1>Photo editor</h1>
       </div>
       <div className="container">
-        <div className="image-gallery">
-          <div className="step-one-container">
-            <p>1</p>
-            <h2>Choose an image</h2>
-          </div>
-          <img className="image-option" alt="option" src={cat_cactus} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={china} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={coast} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={foliage} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={forest} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={plant_shelf} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={tree} onClick={changeImageHandler}></img>
-          <img className="image-option" alt="option" src={windsurfing} onClick={changeImageHandler}></img>
-        </div>
-        <div className="main-image-container">
-          <div className="step-two-container">
-            <p>2</p>
-            <h2>Edit by adjusting filters</h2>
-          </div>
-          <div className="slider-container">
-            {options.map((option, index) => {
-              return(
-                <Slider 
-                key={option.name}
-                name={option.name}
-                min={option.range.min}
-                max={option.range.max}
-                value={option.value}
-                handleChange={handleSliderChange}  
-                />
-                )
-              })}
-            <button className="reset-button" onClick={handleResetButtonClick}>Reset</button>
-          </div>
-          <img id="main-image" src={backgroundUrl} style={getImageStyle()} alt="edited"></img>
-        </div>
+        <ImageGallery changeImageHandler={changeImageHandler}/>
+        <ImageEditor options={options} handleResetButtonClick={handleResetButtonClick} handleSliderChange={handleSliderChange} getImageStyle={getImageStyle} backgroundUrl={backgroundUrl}/>
         <div className="final-step">
           <p>3</p>
           <h2>Cut using snippy/screenshot & crop and use as custom wallpaper</h2>
